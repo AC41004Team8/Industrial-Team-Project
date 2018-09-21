402a39655121a2a9a5695ac1ac3dad8087547100
aaronmc3 <35693220+aaronmc3@users.noreply.github.com>
2018-09-21 14:02:41 +0100

    pre-merge commit lol

--- readcsv.js
+++ readcsv.js
@@ -1,3 +1,7 @@
+/**
+ * Takes a csv specified from url:"" and puts this into an array called lines
+ * 
+ */
 $(document).ready(function() {
     $.ajax({
         type: "GET",
@@ -7,10 +11,12 @@ $(document).ready(function() {
      });
 });
 
+var lines = [];
+
 function processData(allText) {
     var allTextLines = allText.split(/\r\n|\n/);
     var headers = allTextLines[0].split(',');
-    var lines = [];
+    //var lines = [];
 
     for (var i=1; i<allTextLines.length; i++) {
         var data = allTextLines[i].split(',');
@@ -18,13 +24,68 @@ function processData(allText) {
 
             var tarr = [];
             for (var j=0; j<headers.length; j++) {
-                tarr.push(headers[j]+":"+data[j]);
+                //tarr.push(headers[j]+":"+data[j]);
+                tarr.push(data[j]);
             }
+
             lines.push(tarr);
         }
     }
 
-    alert(lines);
+    //alert(lines);
 
     console.log(lines);
+
+    //document.getElementById("line").innerHTML = lines[1].toString();
+    //var big2D;
+
+
+
+    for (var i=1; i<lines.length; i++)
+    {
+        if(lines[i][0] == "800")
+        {
+            array8.push(lines[i]);    
+        }
+        else if(lines[i][0] == "900")
+        {
+            array9.push(lines[i]);    
+        }
+        else if(lines[i][0] == "1000")
+        {
+            array10.push(lines[i]);    
+        }
+        else if(lines[i][0] == "1100")
+        {
+            array11.push(lines[i]);    
+        }
+        else if(lines[i][0] == "1200")
+        {
+            array12.push(lines[i]);    
+        }
+        else if(lines[i][0] == "1300")
+        {
+            array13.push(lines[i]);    
+        }
+        else if(lines[i][0] == "1400")
+        {
+            array14.push(lines[i]);    
+        }
+        else if(lines[i][0] == "1500")
+        {
+            array15.push(lines[i]);    
+        }
+        else if(lines[i][0] == "1600")
+        {
+            array16.push(lines[i]);    
+        }
+        else if(lines[i][0] == "1700")
+        {
+            array17.push(lines[i]);    
+        }
+        
+    }
+    return(lines);
+
+
 }
