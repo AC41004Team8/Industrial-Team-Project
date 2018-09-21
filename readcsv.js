010c789531db82e0439d402ec10242f57cd2b9b4
Johnny McKenzie <jzmckenzie@dundee.ac.uk>
2018-09-21 13:37:53 +0100

    readcsv now works :)

--- readcsv.js
+++ readcsv.js
@@ -3,28 +3,41 @@ $(document).ready(function() {
         type: "GET",
         url: "Dataset.csv",
         dataType: "text",
-        success: function(data) {processData(data);}
+
+        success: function(data) {
+            var processedData = processData(data);
+            var filtered = filterByTime(processedData, 0800);
+            console.log(filtered);
+        }
      });
 });
 
 function processData(allText) {
     var allTextLines = allText.split(/\r\n|\n/);
     var headers = allTextLines[0].split(',');
-    var lines = [];
+    var lines = new Array();
 
     for (var i=1; i<allTextLines.length; i++) {
         var data = allTextLines[i].split(',');
-        if (data.length == headers.length) {
-
-            var tarr = [];
-            for (var j=0; j<headers.length; j++) {
-                tarr.push(headers[j]+":"+data[j]);
-            }
-            lines.push(tarr);
+            lines.push({
+                "Time": data[0],
+                "Id": data[1],
+                "Lat": data[2],
+                "Long": data[3],
+                "Alt": data[4],
+                "FloorIndex": data[5],
+                "Name": data[6],
+                "Footfall": data[7],
+                "Conversion": data[8],
+                "Colour": ""
+            });
         }
-    }
+    return lines;
+}
 
-    alert(lines);
-
-    console.log(lines);
+function filterByTime(processedData, time) {
+    var things = $.grep(processedData, function (shop) {
+        return (shop.Time == time);
+    });
+    return things;
 }
